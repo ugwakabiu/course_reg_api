274d1fcb6b913485be1f5bcc48820f5e577b672f
ugwakabiu <ugwakabiu@gmail.com>
2024-12-21 17:13:07 +0100

    add cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as expressFileUpload from 'express-fileupload';
+import * as cors from 'cors';
 import { static as expressStatic } from 'express';
 import { join as pathJoin } from 'path';
 import { constant } from './constants/constant';
@@ -9,6 +10,7 @@ import { constant } from './constants/constant';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(cors());
   app.use(cookieParser());
   app.use(expressFileUpload());
 
