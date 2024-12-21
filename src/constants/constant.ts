71ac24caee64cedcda2609fa11bc5d6f0c7f8817
ugwakabiu <ugwakabiu@gmail.com>
2024-12-21 12:12:36 +0100

    removing port from constant file

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,5 +1,5 @@
 export const constant = {
-  PORT: process.env.PORT ?? 5000,
+  PORT: process.env.PORT,
   ACCESS_TOKEN_NAME: process.env.ACCESS_TOKEN_NAME ?? 'access-token',
   ACCESS_TOKEN_MAX_AGE: process.env.ACCESS_TOKEN_MAX_AGE ?? '24h',
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ?? 'jwt-secret',
